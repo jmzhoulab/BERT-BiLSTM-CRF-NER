aa11f0d69e53df7b8fa5948e0c44fe6cfeae4783
jmzhoulab <jmzhoulab@sina.cn>
2020-03-27 09:19:24 +0800

    尝试跑一下

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-python run.py -data_dir /Users/zhoujm/workspace/nlp/BERT-BiLSTM-CRF-NER/data1
+python run.py -data_dir data \
+    -bert_config_file pre_model/chinese-bert_chinese_wwm_L-12_H-768_A-12/bert_config.json \
+    -init_checkpoint pre_model/chinese-bert_chinese_wwm_L-12_H-768_A-12/bert_model.ckpt \
+    -vocab_file pre_model/chinese-bert_chinese_wwm_L-12_H-768_A-12/vocab.txt \
+    -output_dir output
